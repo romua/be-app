98a7c619c0396cbd5d5449b3d5b02ab5f79178ec
rchap <romafromukraine@gmail.com>
2019-07-02 14:07:00 +0300

    change files structure

--- be/webpack.config.js
+++ be/webpack.config.js
@@ -1,17 +1,17 @@
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin/dist/clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/app.ts',
   output: {
     filename: 'app.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, './../dist')
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: 'hooks', to: 'public' },
+      { from: './../fe/hooks', to: 'public' },
     ])
   ],
   target: 'node',
